426745de93e59629b5ca0b933026d7327ab996d3
sid-6581 <sid6581@gmail.com>
2022-05-08 17:22:52 -0500

    Automatically install lsp servers

--- lua/user/lsp/configs.lua
+++ lua/user/lsp/configs.lua
@@ -8,7 +8,8 @@ local lspconfig = require("lspconfig")
 local servers = { "jsonls", "sumneko_lua" }
 
 lsp_installer.setup {
-	ensure_installed = servers
+	ensure_installed = servers,
+  automatic_installation = true
 }
 
 for _, server in pairs(servers) do
