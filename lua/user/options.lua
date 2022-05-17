9ac7ae8d3786a3b4c363a4cab8b5866878825db5
sid-6581 <sid6581@gmail.com>
2022-05-16 23:19:00 -0500

    Tweak options

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -14,10 +14,12 @@ o.completeopt = { "menuone", "noselect" }   -- Mostly just for cmp
 o.cursorline = true                         -- Highlight the current line
 o.expandtab = true                          -- Convert tabs to spaces
 o.ffs = { "unix", "dos" }                   -- Prefer editing files as unix files, allow dos
-o.fileformat = "unix"                       -- Start off editing files in unix format
 o.fileencoding = "utf-8"                    -- The encoding written to a file
+o.fileformat = "unix"                       -- Start off editing files in unix format
+o.formatoptions:remove({"c", "r", "o"})     -- Don't insert comments
 o.guifont = "MesloLGS NF:h10"               -- The font used in graphical neovim applications
 o.ignorecase = true                         -- Ignore case in search patterns
+o.iskeyword:append("-")                     -- Make - part of keywords
 o.laststatus = 2                            -- Global statusline
 o.mouse = "a"                               -- Allow the mouse to be used in neovim
 o.number = true                             -- Set numbered lines
@@ -26,6 +28,7 @@ o.pumheight = 10                            -- Pop up menu height
 o.scrolloff = 8                             -- Vertical scroll offset
 o.selection = "inclusive"                   -- Include last character of selection
 o.shiftwidth = 2                            -- The number of spaces inserted for each indentation
+o.shortmess:append("c")                     -- Turn off hit enter prompt for completion popup
 o.showmode = false                          -- We don't need to see things like -- INSERT -- anymore
 o.showtabline = 2                           -- Always show tabs
 o.sidescrolloff = 8                         -- Horizontal scroll offset
@@ -37,24 +40,18 @@ o.splitright = true                         -- Force all vertical splits to go t
 o.swapfile = false                          -- Creates a swapfile
 o.tabstop = 2                               -- Number of spaces for a tab
 o.termguicolors = true                      -- Set term gui colors (most terminals support this)
-o.timeoutlen = 100                          -- Time to wait for a mapped sequence to complete (in milliseconds)
+o.timeoutlen = 400                          -- Time to wait for a mapped sequence to complete (in milliseconds)
 o.updatetime = 300                          -- Faster completion (4000ms default)
+o.virtualedit:append("block")               -- Allow virtual editing in visual block mode
 o.wrap = false                              -- Display lines as one long line
 o.writebackup = false                       -- Don't create backup files
 
--- Neovide
 g.neovide_refresh_rate = 120
 g.neovide_cursor_vfx_mode = "railgun"
-
 g.argtextobj_pairs = "[:],(:),{:},<:>"
-
 g.loaded_ruby_provider = 0
 g.loaded_perl_provider = 0
 
 vim.cmd([[
-  set virtualedit+=block                    " Allow virtual editing in visual block mode
-  set iskeyword+=-                          " Make - part of keywords
-  set shortmess+=c                          " Turn off hit enter prompt for completion popup
-  set formatoptions-=cro                    " Don't insert comments
   cd ~
 ]])
