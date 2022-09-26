02f0e75fc9b78de32cb08aed35159d77187601b1
sid-6581 <sid6581@gmail.com>
2022-09-26 10:31:14 -0500

    Turn off LSP notifications

--- lua/user/notify.lua
+++ lua/user/notify.lua
@@ -91,15 +91,15 @@ vim.lsp.handlers["$/progress"] = function(_, result, ctx)
 end
 
 -- table from lsp severity to vim severity.
-local severity = {
-  "error",
-  "warn",
-  "info",
-  "info", -- map both hint and info to info?
-}
-vim.lsp.handlers["window/showMessage"] = function(err, method, params, client_id)
-  vim.notify(method.message, severity[params.type])
-end
+-- local severity = {
+--   "error",
+--   "warn",
+--   "info",
+--   "info", -- map both hint and info to info?
+-- }
+-- vim.lsp.handlers["window/showMessage"] = function(err, method, params, client_id)
+--   vim.notify(method.message, severity[params.type])
+-- end
 
 notify.setup({
   stages = "fade",
