595e8cd9da6d54620e30cdd6721a074bd72c5dc0
sid-6581 <sid6581@gmail.com>
2022-10-23 12:12:24 -0500

    Set neovide fps to 120

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,5 +1,5 @@
 vim.g.neovide_no_idle = true
-vim.g.neovide_refresh_rate = 60
+vim.g.neovide_refresh_rate = 120
 vim.g.neovide_cursor_vfx_mode = ""
 vim.g.neovide_cursor_animation_length = 0
 vim.g.neovide_remember_window_size = true
