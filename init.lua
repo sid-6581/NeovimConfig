9d9b52d9be2a62fa557d9087bc6804f74565f198
sid-6581 <sid6581@gmail.com>
2022-11-01 21:45:31 -0500

    Change sign column background

--- init.lua
+++ init.lua
@@ -22,8 +22,19 @@ util.safe_require("user.which-key")
 
 local gruvbox = util.safe_require("gruvbox")
 if gruvbox then
+  local palette = require("gruvbox.palette")
   gruvbox.setup({
     contrast = "hard",
+    overrides = {
+      GruvboxAquaSign = { bg = palette.dark0_hard },
+      GruvboxBlueSign = { bg = palette.dark0_hard },
+      GruvboxGreenSign = { bg = palette.dark0_hard },
+      GruvboxOrangeSign = { bg = palette.dark0_hard },
+      GruvboxPurpleSign = { bg = palette.dark0_hard },
+      GruvboxRedSign = { bg = palette.dark0_hard },
+      GruvboxYellowSign = { bg = palette.dark0_hard },
+      SignColumn = { bg = palette.dark0_hard },
+    },
   })
   vim.cmd.colorscheme("gruvbox")
 end
