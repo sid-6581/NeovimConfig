e12a5fcde6d4f8bec14f1b81f088f49874d919a1
sid-6581 <sid6581@gmail.com>
2022-11-02 22:22:42 -0500

    Tweak gitsigns

--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -4,13 +4,6 @@ local gitsigns = util.safe_require("gitsigns")
 if not gitsigns then return end
 
 gitsigns.setup({
-  signs = {
-    add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-    change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-  },
   preview_config = {
     border = "single",
   },
