5d307a551214937e37f5b67eeac085e792ef1c51
sid-6581 <sid6581@gmail.com>
2022-11-08 09:30:54 -0600

    Include dotfiles in telescope find_files

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -71,19 +71,9 @@ telescope.setup({
     },
   },
   pickers = {
-    -- Default configuration for builtin pickers goes here:
-    -- picker_name = {
-    --   picker_config_key = value,
-    --   ...
-    -- }
-    -- Now the picker_config_key will be applied every time you call this
-    -- builtin picker
+    find_files = {
+      find_command = { "fd", "--hidden", "--exclude", ".git", "--glob", "" },
+    },
   },
-  extensions = {
-    -- Your extension configuration goes here:
-    -- extension_name = {
-    --   extension_config_key = value,
-    -- }
-    -- please take a look at the readme of the extension you want to configure
-  },
+  extensions = {},
 })
