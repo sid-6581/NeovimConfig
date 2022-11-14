f7fabd971fc586733afd51143941144111ad5358
sid-6581 <sid6581@gmail.com>
2022-11-13 22:16:29 -0600

    Tweak noice

--- lua/user/noice.lua
+++ lua/user/noice.lua
@@ -12,7 +12,10 @@ noice.setup({
     },
   },
   presets = {
+    bottom_search = true,
+    command_palette = true,
     long_message_to_split = true,
+    inc_rename = false,
     lsp_doc_border = true,
   },
   views = {
