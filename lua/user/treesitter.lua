82c5e3d5cab4015a434690d6daa51bdc202809de
sid-6581 <sid6581@gmail.com>
2022-11-14 16:44:47 -0600

    Disable treesitter folds

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -10,14 +10,6 @@ if vim.fn.has("win32") == 1 then
   install.compilers = { "clang" }
 end
 
-vim.api.nvim_create_autocmd({ "BufEnter", "BufAdd", "BufNew", "BufNewFile", "BufWinEnter" }, {
-  group = vim.api.nvim_create_augroup("TS_FOLD_WORKAROUND", {}),
-  callback = function()
-    vim.opt.foldmethod = "expr"
-    vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-  end,
-})
-
 configs.setup({
   ensure_installed = {
     "bash",
