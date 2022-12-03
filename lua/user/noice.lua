2ea23b172462a31b2596d16b83b11ddc3057ebff
sid-6581 <sid6581@gmail.com>
2022-12-03 14:19:07 -0600

    Tweak noice settings

--- lua/user/noice.lua
+++ lua/user/noice.lua
@@ -4,6 +4,18 @@ local noice = util.safe_require("noice")
 if not noice then return end
 
 noice.setup({
+  cmdline = {
+    view = "cmdline",
+    format = {
+      cmdline = { conceal = false },
+      search_down = { conceal = false },
+      search_up = { conceal = false },
+      filter = { conceal = false },
+      lua = { conceal = false },
+      help = { conceal = false },
+      input = { conceal = false },
+    },
+  },
   lsp = {
     override = {
       ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
@@ -13,7 +25,7 @@ noice.setup({
   },
   presets = {
     bottom_search = true,
-    command_palette = true,
+    command_palette = false,
     long_message_to_split = true,
     inc_rename = false,
     lsp_doc_border = true,
