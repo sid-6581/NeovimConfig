73e95f681119ceafd0cf5c42aacf845121b33daf
sid-6581 <sid6581@gmail.com>
2022-12-24 10:31:25 -0600

    Disable treesitter indent and tweak update check

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -5,18 +5,10 @@ vim.cmd([[
     autocmd TextYankPost * silent! lua vim.highlight.on_yank({ higroup = 'Visual', timeout = 200 }) 
     autocmd BufWinEnter * :set formatoptions-=cro
     autocmd FileType qf set nobuflisted
+    autocmd FocusGained * :checktime
     autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
   augroup end
 ]])
 
--- auto-reload files when modified externally
--- https://unix.stackexchange.com/a/383044
-vim.o.autoread = true
-vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
-  command = "if mode() != 'c' | checktime | endif",
-  pattern = { "*" },
-})
-
 vim.cmd("highlight link FloatBorder Normal")
 vim.cmd("highlight link NormalFloat Normal")
-
--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -80,7 +80,7 @@ function M.config()
       },
     },
     indent = {
-      enable = true,
+      enable = false,
       disable = { "yaml" },
     },
     refactor = {
