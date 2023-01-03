fc902e510c4aad5d2acd06508a032ca24a331986
sid-6581 <sid6581@gmail.com>
2023-01-03 11:22:49 -0600

    Simplify lsp init

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,4 +1,4 @@
-local M = {
+return {
   "neovim/nvim-lspconfig",
   event = { "BufReadPre", "InsertEnter" },
 
@@ -17,53 +17,39 @@ local M = {
       },
     },
   },
+
+  config = function()
+    require("plugins.lsp.diagnostics").setup()
+
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+    capabilities.textDocument.foldingRange = {
+      dynamicRegistration = false,
+      lineFoldingOnly = true,
+    }
+
+    local options = {
+      on_attach = function(client, bufnr)
+        require("plugins.lsp.handlers").on_attach(client, bufnr)
+        require("plugins.lsp.diagnostics").on_attach(client, bufnr)
+        require("plugins.lsp.formatting").on_attach(client, bufnr)
+        require("plugins.lsp.keys").on_attach(client, bufnr)
+      end,
+      capabilities = capabilities,
+      flags = { debounce_text_changes = 150 },
+    }
+
+    require("plugins.lsp.handlers").setup(options)
+    require("plugins.lsp.null-ls").setup(options)
+
+    require("lspconfig.ui.windows").default_options.border = "single"
+    vim.cmd([[
+      highlight link LspInfoBorder Normal
+    ]])
+
+    -- Automatically detect ansible yaml files.
+    vim.cmd([[
+      autocmd BufRead *.yaml,*.yml if search('hosts:\|tasks:', 'nw') | set ft=yaml.ansible | endif
+    ]])
+  end,
 }
-
-local function on_attach(client, bufnr)
-  require("plugins.lsp.handlers").on_attach(client, bufnr)
-  require("plugins.lsp.diagnostics").on_attach(client, bufnr)
-  require("plugins.lsp.formatting").on_attach(client, bufnr)
-  require("plugins.lsp.keys").on_attach(client, bufnr)
-end
-
-function M.config()
-  require("plugins.lsp.diagnostics").setup()
-
-  local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-  capabilities.textDocument.foldingRange = {
-    dynamicRegistration = false,
-    lineFoldingOnly = true,
-  }
-
-  local options = {
-    on_attach = on_attach,
-    capabilities = capabilities,
-    flags = { debounce_text_changes = 150 },
-  }
-
-  require("plugins.lsp.handlers").setup(options)
-  require("plugins.lsp.null-ls").setup(options)
-
-  -- Set LSP info border.
-  require("lspconfig.ui.windows").default_options.border = "single"
-  vim.cmd([[
-    highlight link LspInfoBorder Normal
-  ]])
-
-  -- Automatically detect ansible yaml files.
-  vim.cmd([[
-    autocmd BufRead *.yaml,*.yml if search('hosts:\|tasks:', 'nw') | set ft=yaml.ansible | endif
-  ]])
-
-  -- Automatically format files on save.
-  -- vim.cmd([[
-  --     augroup LspFormat
-  --       autocmd! *
-  --       autocmd BufWritePre *.lua,*.py lua vim.lsp.buf.format()
-  --       autocmd BufWritePre *.tsx,*.ts,*.jsx,*.js,*.vue EslintFixAll
-  --     augroup END
-  -- ]])
-end
-
-return M
