4d2fd85b9a576f8c93af052b12bea2f22eec62e7
sid-6581 <sid6581@gmail.com>
2023-02-23 08:04:59 -0600

    Change rainbow csv

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -28,8 +28,23 @@ return {
   },
 
   {
-    "mechatroner/rainbow_csv",
-    ft = "csv",
+    "cameron-wags/rainbow_csv.nvim",
+    config = true,
+    ft = {
+      "csv",
+      "tsv",
+      "csv_semicolon",
+      "csv_whitespace",
+      "csv_pipe",
+      "rfc_csv",
+      "rfc_semicolon",
+    },
+    cmd = {
+      "RainbowDelim",
+      "RainbowDelimSimple",
+      "RainbowDelimQuoted",
+      "RainbowMultiDelim",
+    },
   },
 
   {
