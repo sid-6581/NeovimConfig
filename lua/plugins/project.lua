4d6f390b96494a0fecf3e25e58b8ee000d581c38
sid-6581 <sid6581@gmail.com>
2023-05-04 09:02:36 -0500

    Tweak project root dir pattern

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -1,11 +1,10 @@
 return {
   "ahmedkhalf/project.nvim",
-  event = "VeryLazy",
 
   config = function()
     require("project_nvim").setup({
       ignore_lsp = { "null-ls" },
-      patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", ".gitignore" },
+      patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", ".gitignore", "*" },
       scope_chdir = "tab",
       show_hidden = true,
       silent_chdir = true,
