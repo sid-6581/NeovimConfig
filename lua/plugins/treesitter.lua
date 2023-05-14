0fd21f379616e43a0856611697b0c61ddb5e5d82
sid-6581 <sid6581@gmail.com>
2023-05-14 11:10:55 -0500

    Don't prefer git for treesitter install

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -18,7 +18,7 @@ local M = {
 function M.config()
   if vim.fn.has("win32") == 1 then require("nvim-treesitter.install").compilers = { "clang" } end
 
-  require("nvim-treesitter.install").prefer_git = true
+  require("nvim-treesitter.install").prefer_git = false
 
   require("nvim-treesitter.configs").setup({
     ensure_installed = {
