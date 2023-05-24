dd69427c1246e85df7e648a350b731aed23e5627
sid-6581 <sid6581@gmail.com>
2023-05-24 14:19:12 -0500

    Tweak keymaps

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -32,7 +32,6 @@ return {
     {
       "mg979/vim-visual-multi",
       init = function()
-        -- vim.g.VM_quit_after_leaving_insert_mode = 1
         vim.g.VM_show_warnings = 0
         vim.g.VM_silent_exit = 1
       end,
