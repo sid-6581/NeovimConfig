e215aee40d3d94d2a3570a160bdf9c09a1aafb20
sid-6581 <sid6581@gmail.com>
2023-06-06 23:06:48 -0500

    Use treesitter folding

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -31,9 +31,12 @@ return {
       table.insert(newVirtText, { suffix, "MoreMsg" })
       return newVirtText
     end
+
     require("ufo").setup({
       fold_virt_text_handler = handler,
+      provider_selector = function() return { "treesitter", "indent" } end,
     })
+
     vim.keymap.set("n", "zR", require("ufo").openAllFolds)
     vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
     vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
