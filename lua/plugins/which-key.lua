c83bad9804df1c1c57a0d97816aa1002e873e6b2
sid-6581 <sid6581@gmail.com>
2023-06-28 10:08:32 -0500

    Tweak flash

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -33,10 +33,8 @@ return {
     {
       "folke/flash.nvim",
       opts = {
-        highlight = {
-          label = {
-            style = "inline",
-          },
+        label = {
+          style = "inline",
         },
         modes = {
           search = {
