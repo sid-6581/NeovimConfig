2b05418fe34a0d0e05afc06ca8e0c79c76eb429d
sid-6581 <sid6581@gmail.com>
2023-07-27 12:32:07 -0500

    Add nvim-spider

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -97,6 +97,18 @@ return {
   },
 
   {
+    "chrisgrieser/nvim-spider",
+    event = "VeryLazy",
+    keys = {
+      { mode = { "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", desc = "Spider-w" },
+      { mode = { "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", desc = "Spider-e" },
+      { mode = { "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", desc = "Spider-b" },
+      { mode = { "n", "o", "x" }, "ge", "<cmd>lua require('spider').motion('ge')<CR>", desc = "Spider-ge" },
+    },
+    opts = {},
+  },
+
+  {
     "folke/flash.nvim",
     event = "VeryLazy",
 
