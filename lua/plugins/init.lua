4f701969c7e5d6f82a007ef793e6b37ca8fb2483
sid-6581 <sid6581@gmail.com>
2023-07-27 14:41:32 -0500

    Use spider only in normal mode

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -101,10 +101,10 @@ return {
     event = "VeryLazy",
 
     keys = {
-      { mode = { "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", desc = "Spider-w" },
-      { mode = { "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", desc = "Spider-e" },
-      { mode = { "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", desc = "Spider-b" },
-      { mode = { "n", "o", "x" }, "ge", "<cmd>lua require('spider').motion('ge')<CR>", desc = "Spider-ge" },
+      { "w", "<cmd>lua require('spider').motion('w')<CR>", desc = "Spider-w" },
+      { "e", "<cmd>lua require('spider').motion('e')<CR>", desc = "Spider-e" },
+      { "b", "<cmd>lua require('spider').motion('b')<CR>", desc = "Spider-b" },
+      { "ge", "<cmd>lua require('spider').motion('ge')<CR>", desc = "Spider-ge" },
     },
 
     opts = {},
