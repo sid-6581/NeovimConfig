d03a6e501d0777e0c69d2da06c9c322334268bdc
sid-6581 <sid6581@gmail.com>
2023-09-08 17:37:58 -0500

    Tweak cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -55,10 +55,6 @@ return {
     local function modified_kind(kind) return modified_priority[kind] or kind end
 
     cmp.setup({
-      completion = {
-        completeopt = "menu,menuone,noselect",
-      },
-
       snippet = {
         expand = function(args) require("luasnip").lsp_expand(args.body) end,
       },
