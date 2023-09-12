407909e79a5e56f88966f4340286e7b5c4fb54b8
sid-6581 <sid6581@gmail.com>
2023-09-11 21:15:32 -0500

    Fix cmp border

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -125,8 +125,8 @@ return {
       }),
 
       window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
+        completion = cmp.config.window.bordered({ border = "single", winhighlight = "" }),
+        documentation = cmp.config.window.bordered({ border = "single", winhighlight = "" }),
       },
 
       experimental = {
