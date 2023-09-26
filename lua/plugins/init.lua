d3097b44a63eac6f5d1b8c55378cb5503c9645c3
sid-6581 <sid6581@gmail.com>
2023-09-26 15:15:28 -0500

    Add dial.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -342,4 +342,20 @@ return {
       vim.api.nvim_set_hl(0, "YankyYanked", { link = "Visual" })
     end,
   },
+
+  {
+    "monaqa/dial.nvim",
+    event = "VeryLazy",
+
+    keys = {
+      { "n", "<C-a>", function() require("dial.map").manipulate("increment", "normal") end },
+      { "n", "<C-x>", function() require("dial.map").manipulate("decrement", "normal") end },
+      { "n", "g<C-a>", function() require("dial.map").manipulate("increment", "gnormal") end },
+      { "n", "g<C-x>", function() require("dial.map").manipulate("decrement", "gnormal") end },
+      { "v", "<C-a>", function() require("dial.map").manipulate("increment", "visual") end },
+      { "v", "<C-x>", function() require("dial.map").manipulate("decrement", "visual") end },
+      { "v", "g<C-a>", function() require("dial.map").manipulate("increment", "gvisual") end },
+      { "v", "g<C-x>", function() require("dial.map").manipulate("decrement", "gvisual") end },
+    },
+  },
 }
