a2b66b41c4ff6bce3d2f70521814d83e01f31c1e
sid-6581 <sid6581@gmail.com>
2023-10-15 22:32:16 -0500

    Move which-key

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,35 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+
+  opts = {
+    window = {
+      border = "single", -- none, single, double, shadow
+      winblend = 10,
+    },
+    icons = {
+      group = "",
+    },
+    defaults = {
+      ["gp"] = { name = "Goto Preview" },
+      ["]"] = { name = "Next" },
+      ["["] = { name = "Prev" },
+      ["<Leader><tab>"] = { name = "Tabs" },
+      ["<Leader>b"] = { name = "Buffers" },
+      ["<Leader>c"] = { name = "Code" },
+      ["<Leader>d"] = { name = "Debug" },
+      ["<Leader>f"] = { name = "Find" },
+      ["<Leader>l"] = { name = "LSP" },
+      ["<Leader>g"] = { name = "Git" },
+      ["<Leader>t"] = { name = "Terminal" },
+      ["<Leader>u"] = { name = "Toggles" },
+      ["<Leader>w"] = { name = "Windows" },
+      ["<Leader>x"] = { name = "Diagnostics/Quickfix" },
+    },
+  },
+
+  config = function(_, opts)
+    require("which-key").setup(opts)
+    require("which-key").register(opts.defaults)
+  end,
+}
