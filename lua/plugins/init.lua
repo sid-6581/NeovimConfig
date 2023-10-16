528cf40ceab87fdcc4aab7755043d2f715594885
sid-6581 <sid6581@gmail.com>
2023-10-15 22:36:48 -0500

    Move close-buffers

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -233,30 +233,4 @@ return {
 
     opts = {},
   },
-
-  {
-    "kazhala/close-buffers.nvim",
-    event = "VeryLazy",
-
-    keys = {
-      { "<S-Esc>", function() require("close_buffers").delete({ type = "this" }) end, desc = "Delete buffer" },
-      { "<Leader>bd", function() require("close_buffers").delete({ type = "this" }) end, desc = "Delete buffer" },
-      { "<Leader>ba", function() require("close_buffers").delete({ type = "all" }) end, desc = "Delete all buffers" },
-      {
-        "<Leader>bh",
-        function() require("close_buffers").delete({ type = "hidden" }) end,
-        desc = "Delete hidden buffers",
-      },
-      {
-        "<Leader>bo",
-        function() require("close_buffers").delete({ type = "other" }) end,
-        desc = "Delete other buffers",
-      },
-    },
-
-    opts = {
-      filetype_ignore = { "neo-tree" },
-      preserve_window_layout = { "this" },
-    },
-  },
 }
