a1faa027a1cb212c0b15a37d712254e94ba1f7fb
sid-6581 <sid6581@gmail.com>
2023-10-15 22:36:15 -0500

    Move trouble

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -259,30 +259,4 @@ return {
       preserve_window_layout = { "this" },
     },
   },
-
-  {
-    "folke/trouble.nvim",
-    event = "VeryLazy",
-
-    opts = {
-      use_diagnostic_signs = true,
-    },
-
-    keys = {
-      { "<Leader>xx", function() require("trouble").toggle() end, desc = "Toggle trouble" },
-      {
-        "<Leader>xw",
-        function() require("trouble").toggle("workspace_diagnostics") end,
-        desc = "Toggle workspace diagnostics",
-      },
-      {
-        "<Leader>xd",
-        function() require("trouble").toggle("document_diagnostics") end,
-        desc = "Toggle document diagnostics",
-      },
-      { "<Leader>xl", function() require("trouble").toggle("loclist") end, desc = "Toggle loc list" },
-      { "<Leader>xq", function() require("trouble").toggle("quickfix") end, desc = "Toggle quickfix" },
-      { "<Leader>xr", function() require("trouble").toggle("lsp_references") end, desc = "Toggle references" },
-    },
-  },
 }
