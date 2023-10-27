ad68438cccf87ee49f54ff1223216c6cd8ca4e68
sid-6581 <sid6581@gmail.com>
2023-10-26 22:32:05 -0500

    Fix foldingRange

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -101,6 +101,10 @@ return {
 
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+    capabilities.textDocument.foldingRange = {
+      dynamicRegistration = false,
+      lineFoldingOnly = true,
+    }
 
     local options = {
       on_attach = function(client, bufnr)
