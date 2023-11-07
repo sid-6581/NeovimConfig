1fe96ce8d93905c105b8823b0864269800530998
sid-6581 <sid6581@gmail.com>
2023-11-07 09:54:27 -0600

    Fix msys2 current directory

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -17,7 +17,7 @@ return {
 
   config = function()
     local shell = (vim.fn.has("win32") == 1)
-        and "C:/msys64/msys2_shell.cmd -defterm -no-start -ucrt64 -full-path -shell zsh"
+        and "C:/msys64/msys2_shell.cmd -defterm -here -no-start -ucrt64 -full-path -shell zsh"
       or vim.o.shell
 
     require("toggleterm").setup({
