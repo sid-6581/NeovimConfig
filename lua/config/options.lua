325e82c7d4a4beaa5992ea6b29adcf0d3a1bf0b3
sid-6581 <sid6581@gmail.com>
2023-11-08 14:40:01 -0600

    Enable drop shadows

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,7 +11,7 @@ vim.g.neovide_cursor_vfx_mode = ""
 vim.g.neovide_no_idle = true
 vim.g.neovide_refresh_rate = 120
 vim.g.neovide_remember_window_size = true
-vim.g.neovide_floating_shadow = false
+vim.g.neovide_floating_shadow = true
 
 vim.opt.autowrite = true
 vim.opt.backup = true
