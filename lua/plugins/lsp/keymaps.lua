3535a111f8b463d3b32f01d10625b0432b3a518f
sid-6581 <sid6581@gmail.com>
2023-11-20 14:46:54 -0600

    Tweak keymaps

--- lua/plugins/lsp/keymaps.lua
+++ lua/plugins/lsp/keymaps.lua
@@ -26,7 +26,7 @@ function M.on_attach(_client, buffer)
   map("n", "<Leader>ls", function() require("telescope.builtin").lsp_dynamic_workspace_symbols() end, { desc = "Workspace symbols" })
   map("n", "K", function() vim.lsp.buf.hover() end, { desc = "Show information" })
   map("n", "gK", function() vim.lsp.buf.signature_help() end, { desc = "Signature Help" })
-  map("n", "gd", function() require("telescope.builtin").lsp_definitions() end, { desc = "Go to definition symbols" })
+  map("n", "gd", function() vim.lsp.buf.definition() end, { desc = "Go to definition" })
   map("v", "<Leader>cf", function() require("plugins.lsp.formatting").format() end, { desc = "Format range" })
   map({ "n", "i" }, "<A-Enter>", function() vim.lsp.buf.code_action() end, { desc = "Code action" })
 
