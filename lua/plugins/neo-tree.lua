42a9cfc1ecb79a827e7809d71935dee137629cc0
sid-6581 <sid6581@gmail.com>
2023-12-05 13:13:36 -0600

    Force cwd in neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,7 +7,7 @@ return {
     { "<A-1>", "<CMD>Neotree toggle reveal_force_cwd<CR>", desc = "File explorer" },
     { "<A-2>", "<CMD>Neotree toggle show buffers<CR>", desc = "Buffer explorer" },
     { "<A-3>", "<CMD>Neotree toggle show git_status<CR>", desc = "Git explorer" },
-    { "<Leader>E", "<CMD>Neotree reveal<CR>", desc = "Find file in explorer" },
+    { "<Leader>E", "<CMD>Neotree reveal_force_cwd<CR>", desc = "Find file in explorer" },
   },
 
   opts = {
