cd7e20fbab90dbfd6a4285550a7e82c0c9079b1b
sid-6581 <sid6581@gmail.com>
2023-12-04 19:45:48 -0600

    Tweak oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,17 +1,18 @@
 return {
   "stevearc/oil.nvim",
-  opts = {},
-
-  config = function(_, opts)
-    require("oil").setup(opts)
-
-    vim.api.nvim_create_autocmd("TextChanged", {
-      desc = "Set cwd to follow directory shown in oil buffers",
-      group = vim.api.nvim_create_augroup("OilAutoCwd", {}),
-      pattern = "oil:///*",
-      callback = function()
-        if vim.bo.filetype == "oil" then vim.cmd.lcd(require("oil").get_current_dir()) end
-      end,
-    })
-  end,
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+    float = {
+      padding = 15,
+      border = "single",
+    },
+    preview = {
+      border = "single",
+    },
+    progress = {
+      border = "single",
+    },
+  },
 }
