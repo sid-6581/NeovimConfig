75748454bfbd35e558b66c471c822ff2ad3b0107
sid-6581 <sid6581@gmail.com>
2024-01-10 08:55:44 -0600

    Fix mini.misc init event

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -6,6 +6,7 @@ return {
 
   {
     "echasnovski/mini.misc",
+    event = "VimEnter",
 
     config = function()
       require("mini.misc").setup_auto_root(nil, function(path) return vim.fs.dirname(path) end)
