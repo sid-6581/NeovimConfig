2920cee2ba8b0811f89b7c373a55654356f60b08
sid-6581 <sid6581@gmail.com>
2024-02-18 19:39:28 -0600

    Redirect all prints to vim.notify and remove guess-indent

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -89,3 +89,14 @@ vim.filetype.add({
     nomad = "hcl",
   },
 })
+
+-- Redirect all prints to vim.notify
+---@diagnostic disable-next-line: missing-global-doc
+print = function(...)
+  local print_safe_args = {}
+  local _ = { ... }
+  for i = 1, #_ do
+    table.insert(print_safe_args, tostring(_[i]))
+  end
+  vim.notify(table.concat(print_safe_args, " "), "info")
+end
