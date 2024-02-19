fbf2a47ad3359ca47bfb645c8e3f52155177b4ad
sid-6581 <sid6581@gmail.com>
2024-02-19 15:49:59 -0600

    Hide rust-analyzer progress

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -1,6 +1,22 @@
 return {
   "j-hui/fidget.nvim",
   opts = {
+    progress = {
+      ignore_done_already = true,
+      ignore_empty_message = true,
+      display = {
+        overrides = {
+          rust_analyzer = {
+            update_hook = function(item)
+              require("fidget.notification").set_content_key(item)
+              if item.annote:match("clippy") then item.hidden = true end
+              if item.annote:match("Roots") then item.hidden = true end
+              if item.annote:match("Loading") then item.hidden = true end
+            end,
+          },
+        },
+      },
+    },
     notification = {
       filter = vim.log.levels.DEBUG,
       override_vim_notify = true,
