2aa34308f385a184b74dc05615daae669ee46063
sid-6581 <sid6581@gmail.com>
2024-02-26 15:08:39 -0600

    Tweak symbol-usage

--- lua/plugins/symbol-usage.lua
+++ lua/plugins/symbol-usage.lua
@@ -1,7 +1,17 @@
+local SymbolKind = vim.lsp.protocol.SymbolKind
+
 return {
   "Wansmer/symbol-usage.nvim",
   event = "VeryLazy",
   opts = {
+    kinds = {
+      SymbolKind.Function,
+      SymbolKind.Method,
+      SymbolKind.Field,
+      SymbolKind.Struct,
+      SymbolKind.Class,
+      SymbolKind.Interface,
+    },
     vt_position = "end_of_line",
     request_pending_text = false,
   },
