df54c043ba613616c07a05f54ee5406ab8dad1d4
sid-6581 <sid6581@gmail.com>
2024-03-20 11:15:35 -0500

    Move keybinds from mini.surround to nvim-surround

---
+++ lua/plugins/nvim-surround.lua
@@ -0,0 +1,32 @@
+return {
+  "kylechui/nvim-surround",
+  event = "VeryLazy",
+
+  keys = {
+    { "<Leader>'", "csq'", desc = "Replace surround with ''", remap = true },
+    { "<Leader>(", "ds(", desc = "Remove surrounding ()", remap = true },
+    { "<Leader>)", "csb)", desc = "Replace surround with ()", remap = true },
+    { "<Leader><", "ds<", desc = "Remove surrounding <>", remap = true },
+    { "<Leader>>", "csb>", desc = "Replace surround with <>", remap = true },
+    { "<Leader>[", "ds[", desc = "Remove surrounding []", remap = true },
+    { "<Leader>]", "csb]", desc = "Replace surround with []", remap = true },
+    { "<Leader>{", "ds{", desc = "Remove surrounding {}", remap = true },
+    { "<Leader>}", "csb{", desc = "Replace surround with { }", remap = true },
+    { '<Leader>"', 'csq"', desc = 'Replace surround with ""', remap = true },
+    { mode = { "v" }, "<Leader>'", "S'", desc = "Surround with ''", remap = true },
+    { mode = { "v" }, "<Leader>)", "S)", desc = "Surround with ()", remap = true },
+    { mode = { "v" }, "<Leader>>", "S>", desc = "Surround with <>", remap = true },
+    { mode = { "v" }, "<Leader>]", "S]", desc = "Surround with []", remap = true },
+    { mode = { "v" }, "<Leader>}", "S{", desc = "Surround with { }", remap = true },
+    { mode = { "v" }, '<Leader>"', 'S"', desc = 'Surround with ""', remap = true },
+  },
+
+  opts = {
+    aliases = {
+      ["b"] = { ")", "]", "}" },
+      ["q"] = { '"', "'", "`" },
+      ["s"] = { "}", "]", ")", ">", '"', "'", "`" },
+    },
+    move_cursor = false,
+  },
+}
