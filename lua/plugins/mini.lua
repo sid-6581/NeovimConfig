522e155752003a23f9e7dbb25b07e2863f9cab47
sid-6581 <sid6581@gmail.com>
2024-04-05 12:38:10 -0500

    Add higlight patterns

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -178,6 +178,10 @@ return {
 
     opts = {
       highlighters = {
+        fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+        hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+        todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+        note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
         scriban_block = {
           pattern = function(_buf_id)
             if vim.fn.expand("%:e") ~= "sbntxt" then return nil end
