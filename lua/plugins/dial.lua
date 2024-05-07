18891595edac499a10a98cf5ce6ac86f754a6cbf
sid-6581 <sid6581@gmail.com>
2024-05-06 22:24:57 -0500

    Add descriptions to dial keymaps

--- lua/plugins/dial.lua
+++ lua/plugins/dial.lua
@@ -3,13 +3,33 @@ return {
   event = "VeryLazy",
 
   keys = {
-    { "<C-A>", function() require("dial.map").manipulate("increment", "normal") end },
-    { "<C-X>", function() require("dial.map").manipulate("decrement", "normal") end },
-    { "g<C-A>", function() require("dial.map").manipulate("increment", "gnormal") end },
-    { "g<C-X>", function() require("dial.map").manipulate("decrement", "gnormal") end },
-    { mode = { "v" }, "<C-A>", function() require("dial.map").manipulate("increment", "visual") end },
-    { mode = { "v" }, "<C-X>", function() require("dial.map").manipulate("decrement", "visual") end },
-    { mode = { "v" }, "g<C-A>", function() require("dial.map").manipulate("increment", "gvisual") end },
-    { mode = { "v" }, "g<C-X>", function() require("dial.map").manipulate("decrement", "gvisual") end },
+    { "<C-A>", function() require("dial.map").manipulate("increment", "normal") end, desc = "Increment" },
+    { "<C-X>", function() require("dial.map").manipulate("decrement", "normal") end, desc = "Decrement" },
+    { "g<C-A>", function() require("dial.map").manipulate("increment", "gnormal") end, desc = "Increment" },
+    { "g<C-X>", function() require("dial.map").manipulate("decrement", "gnormal") end, desc = "Decrement" },
+    {
+      mode = { "v" },
+      "<C-A>",
+      function() require("dial.map").manipulate("increment", "visual") end,
+      desc = "Increment",
+    },
+    {
+      mode = { "v" },
+      "<C-X>",
+      function() require("dial.map").manipulate("decrement", "visual") end,
+      desc = "Decrement",
+    },
+    {
+      mode = { "v" },
+      "g<C-A>",
+      function() require("dial.map").manipulate("increment", "gvisual") end,
+      desc = "Increment",
+    },
+    {
+      mode = { "v" },
+      "g<C-X>",
+      function() require("dial.map").manipulate("decrement", "gvisual") end,
+      desc = "Decrement",
+    },
   },
 }
