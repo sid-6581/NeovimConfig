a689d50035612f140a4b8e0c8590691b539e48a8
sid-6581 <sid6581@gmail.com>
2024-05-17 09:38:26 -0500

    Hide deprecation messages

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -28,10 +28,24 @@ return {
     notification = {
       filter = vim.log.levels.DEBUG,
       override_vim_notify = true,
+      view = {
+        stack_upwards = false,
+      },
       window = {
         winblend = 0,
         x_padding = 0,
         y_padding = 0,
+      },
+      configs = {
+        default = {
+          update_hook = function(item)
+            require("fidget.notification").set_content_key(item)
+            -- Hide deprecation related messages.
+            if item.message:match("tbl_islist") then item.hidden = true end
+            if item.message:match("tbl_add_reverse_lookup") then item.hidden = true end
+            if item.message:match("diagnostic.is_disabled") then item.hidden = true end
+          end,
+        },
       },
     },
   },
