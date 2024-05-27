7134bb7202fcda8a03ffb05d2c2beecafc1ec8a7
sid-6581 <sid6581@gmail.com>
2024-05-26 20:32:40 -0500

    Tweak mini.misc priority

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -6,7 +6,7 @@ return {
 
   {
     "echasnovski/mini.misc",
-    event = "VimEnter",
+    priority = 1000,
 
     config = function()
       require("mini.misc").setup_auto_root(nil, function(path) return vim.fs.dirname(path) end)
