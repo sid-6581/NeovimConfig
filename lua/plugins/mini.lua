b2120533fc00f27db1c6a05e3c8708081748193b
sid-6581 <sid6581@gmail.com>
2024-05-28 11:51:54 -0500

    Remove mini.surround

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -219,43 +219,4 @@ return {
       }
     end,
   },
-
-  {
-    "echasnovski/mini.surround",
-    event = "VeryLazy",
-
-    -- keys = {
-    --   { "<Leader>'", "<Leader>srq'", desc = "Replace surround with ''", remap = true },
-    --   { "<Leader>(", "<Leader>sd)", desc = "Remove surrounding ()", remap = true },
-    --   { "<Leader>)", "<Leader>srb)", desc = "Replace surround with ()", remap = true },
-    --   { "<Leader><", "<Leader>sd>", desc = "Remove surrounding <>", remap = true },
-    --   { "<Leader>>", "<Leader>srb>", desc = "Replace surround with <>", remap = true },
-    --   { "<Leader>[", "<Leader>sd]", desc = "Remove surrounding []", remap = true },
-    --   { "<Leader>]", "<Leader>srb]", desc = "Replace surround with []", remap = true },
-    --   { "<Leader>{", "<Leader>sd{", desc = "Remove surrounding {}", remap = true },
-    --   { "<Leader>}", "<Leader>srb{", desc = "Replace surround with { }", remap = true },
-    --   { '<Leader>"', '<Leader>srq"', desc = 'Replace surround with ""', remap = true },
-    --   { "<Leader>'", "<Leader>sa'", desc = "Surround with ''", mode = { "v" }, remap = true },
-    --   { "<Leader>)", "<Leader>sa)", desc = "Surround with ()", mode = { "v" }, remap = true },
-    --   { "<Leader>>", "<Leader>sa>", desc = "Surround with <>", mode = { "v" }, remap = true },
-    --   { "<Leader>]", "<Leader>sa]", desc = "Surround with []", mode = { "v" }, remap = true },
-    --   { "<Leader>}", "<Leader>sa{", desc = "Surround with { }", mode = { "v" }, remap = true },
-    --   { '<Leader>"', '<Leader>sa"', desc = 'Surround with ""', mode = { "v" }, remap = true },
-    -- },
-
-    opts = {
-      mappings = {
-        add = "<Leader>sa",
-        delete = "<Leader>sd",
-        find = "<Leader>sf",
-        find_left = "<Leader>sF",
-        highlight = "<Leader>sh",
-        replace = "<Leader>sr",
-        update_n_lines = "<Leader>sn",
-        suffix_last = "l",
-        suffix_next = "n",
-      },
-      respect_selection_type = true,
-    },
-  },
 }
