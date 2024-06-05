81668896c639c01fcb9b6a65d52e16560e38a8b3
sid-6581 <sid6581@gmail.com>
2024-06-04 22:05:20 -0500

    Fix cliff mappings

--- lua/plugins/cliff.lua
+++ lua/plugins/cliff.lua
@@ -4,6 +4,6 @@ return {
 
   keys = {
     { "<S-A-j>", function() require("cliff").go_down() end, mode = { "n", "v", "o" }, desc = "Cliff down" },
-    { "<S-A-k>", function() require("cliff").go_up() end, mode = { "n", "v", "o" }, "Cliff up" },
+    { "<S-A-k>", function() require("cliff").go_up() end, mode = { "n", "v", "o" }, desc = "Cliff up" },
   },
 }
