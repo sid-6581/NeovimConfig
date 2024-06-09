30199bc080f6476a2b4b6d10c55dc0234cc389bb
sid-6581 <sid6581@gmail.com>
2024-06-09 18:42:30 -0500

    Tweak context

--- lua/plugins/nvim-context-vt.lua
+++ lua/plugins/nvim-context-vt.lua
@@ -2,8 +2,14 @@ return {
   "andersevenrud/nvim_context_vt",
   event = "VeryLazy",
 
+  keys = {
+    { "<Leader>ut", "<CMD>NvimContextVtToggle<CR>", desc = "Toggle treesitter context" },
+  },
+
   opts = {
     prefix = "",
+    highlight = "GruvboxBg3",
     disable_virtual_lines = true,
+    min_rows = 5,
   },
 }
