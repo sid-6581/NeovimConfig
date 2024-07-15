bffa778087daaefca3055f9a3cb0cbf0f9f20672
sid-6581 <sid6581@gmail.com>
2024-07-15 17:00:35 -0500

    Fix which-key icon translations

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -3,7 +3,7 @@ return {
   event = "VeryLazy",
 
   opts = {
-    preset = "modern",
+    preset = "helix",
     sort = { "order", "group", "alphanum", "mod", "lower", "icase" },
     win = {
       wo = {
@@ -12,6 +12,37 @@ return {
     },
     icons = {
       separator = " ",
+      group = "",
+    },
+    replace = {
+      key = {
+        function(lhs)
+          local Util = require("which-key.util")
+          local Config = require("which-key.config")
+          local keys = Util.keys(lhs)
+          local ret = vim.tbl_map(function(key)
+            local inner = key:match("^<(.*)>$")
+            if not inner then
+              return key
+            end
+
+            if inner == "NL" then
+              inner = "C-J"
+            end
+
+            local parts = vim.split(inner, "-", { plain = true })
+            parts[1] = Config.icons.keys[parts[1]] or parts[1]
+            if parts[2] and parts[3] then
+              vim.print(key)
+              vim.print(parts[2])
+              parts[2] = Config.icons.keys[parts[2]] or parts[2]
+            end
+
+            return table.concat(parts, "")
+          end, keys)
+          return table.concat(ret, "")
+        end,
+      },
     },
     spec = {
       { "<Leader>", group = "Leader" },
