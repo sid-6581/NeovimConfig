b123444e40cf7c7e5bc43035365ee1c0d51d87f8
sid-6581 <sid6581@gmail.com>
2024-07-24 15:23:38 -0500

    Remove popup override for crates

--- lua/plugins/crates.lua
+++ lua/plugins/crates.lua
@@ -8,20 +8,6 @@ return {
       actions = true,
       completion = true,
       hover = true,
-      on_attach = function(_client, bufnr)
-        require("which-key").add({
-          "K",
-          function()
-            if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
-              require("crates").show_popup()
-            else
-              vim.lsp.buf.hover()
-            end
-          end,
-          buffer = bufnr,
-          desc = "Show information [crates]"
-        })
-      end,
     },
     popup = {
       border = "single",
