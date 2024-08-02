b35c231c2b050fcca7926d77cfd9a016839c09cb
sid-6581 <sid6581@gmail.com>
2024-08-01 20:47:05 -0500

    Tweak mini.files keymaps

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -95,6 +95,12 @@ return {
     },
 
     opts = {
+      mappings = {
+        go_in = "L",
+        go_in_plus = "l",
+        show_help = "?",
+        synchronize = "<C-S>",
+      },
       windows = {
         preview = true,
         width_focus = 50,
@@ -104,6 +110,38 @@ return {
         use_as_default_explorer = false,
       },
     },
+
+    config = function(_, opts)
+      require("mini.files").setup(opts)
+
+      local map_split = function(buf_id, lhs, direction)
+        local rhs = function()
+          -- Make new window and set it as target
+          local new_target_window
+          --- @diagnostic disable-next-line: param-type-mismatch
+          vim.api.nvim_win_call(require("mini.files").get_target_window(), function()
+            vim.cmd(direction .. " split")
+            new_target_window = vim.api.nvim_get_current_win()
+          end)
+
+          require("mini.files").set_target_window(new_target_window)
+          require("mini.files").close()
+        end
+
+        -- Adding `desc` will result into `show_help` entries
+        local desc = "Split " .. direction .. " [mini.files]"
+        vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
+      end
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "MiniFilesBufferCreate",
+        callback = function(args)
+          local buf_id = args.data.buf_id
+          map_split(buf_id, "-", "horizontal")
+          map_split(buf_id, "\\", "vertical")
+        end,
+      })
+    end,
   },
 
   {
