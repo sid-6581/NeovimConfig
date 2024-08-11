89c5f0194a9df6f1b676610a239508b8617510f5
sid-6581 <sid6581@gmail.com>
2024-08-11 17:16:53 -0500

    Use markview

---
+++ lua/plugins/markview.lua
@@ -0,0 +1,16 @@
+return {
+  "OXY2DEV/markview.nvim",
+  event = "VeryLazy",
+
+  opts = {
+    modes = { "n", "i", "no", "c" },
+    hybrid_modes = { "i" },
+
+    callbacks = {
+      on_enable = function(_, win)
+        vim.wo[win].conceallevel = 2
+        vim.wo[win].concealcursor = "c"
+      end,
+    },
+  },
+}
