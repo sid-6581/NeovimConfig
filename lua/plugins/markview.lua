5340855db70eba627c152d9908250782a43c05b6
sid-6581 <sid6581@gmail.com>
2024-08-11 17:17:18 -0500

    Tweak markview

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -9,7 +9,7 @@ return {
     callbacks = {
       on_enable = function(_, win)
         vim.wo[win].conceallevel = 2
-        vim.wo[win].concealcursor = "c"
+        vim.wo[win].concealcursor = "nc"
       end,
     },
   },
