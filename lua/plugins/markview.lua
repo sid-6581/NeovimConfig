ab4b3ec65a2610d0472e4f1ac0207796ac2010ef
sid-6581 <sid6581@gmail.com>
2024-08-13 18:10:22 -0500

    Fix markview lazy event

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,16 +1,6 @@
 return {
   "OXY2DEV/markview.nvim",
-  event = "VeryLazy",
+  event = { "VeryLazy", "BufReadPre" },
 
-  opts = {
-    -- modes = { "n", "no" },
-    -- hybrid_modes = { "i" },
-
-    -- callbacks = {
-    --   on_enable = function(_, win)
-    --     vim.wo[win].conceallevel = 2
-    --     vim.wo[win].concealcursor = "c"
-    --   end,
-    -- },
-  },
+  opts = {},
 }
