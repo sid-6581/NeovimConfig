1a3536963ad6761e443c8934e8077f87b3f9fcc2
sid-6581 <sid6581@gmail.com>
2024-08-15 11:14:20 -0500

    Change markview headers

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -3,5 +3,27 @@ return {
   event = "VeryLazy",
   ft = "markdown",
 
-  opts = {},
+  opts = {
+    headings = {
+      shift_width = 0,
+      heading_1 = {
+        icon = "󰉫 ",
+      },
+      heading_2 = {
+        icon = "󰉬 ",
+      },
+      heading_3 = {
+        icon = "󰉭 ",
+      },
+      heading_4 = {
+        icon = "󰉮 ",
+      },
+      heading_5 = {
+        icon = "󰉯 ",
+      },
+      heading_6 = {
+        icon = "󰉰 ",
+      },
+    },
+  },
 }
