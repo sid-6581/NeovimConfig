d19759f666967a90fa4e5d0c26fde42b96341ea1
sid-6581 <sid6581@gmail.com>
2024-08-19 11:49:42 -0500

    Tweak lazydev setup

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -3,25 +3,27 @@ return {
     "folke/lazydev.nvim",
     event = "VeryLazy",
 
-    dependencies = {
-      {
-        "hrsh7th/nvim-cmp",
-        opts = function(_, opts)
-          opts.sources = opts.sources or {}
-          table.insert(opts.sources, {
-            name = "lazydev",
-            group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-          })
-        end,
-      },
-
-      { "Bilal2453/luvit-meta", lazy = true },
-    },
-
     opts = {
       library = {
         "luvit-meta/library",
       },
     },
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, {
+        name = "lazydev",
+        group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+      })
+    end,
+  },
+
+  {
+    "Bilal2453/luvit-meta",
+    lazy = true,
   },
 }
