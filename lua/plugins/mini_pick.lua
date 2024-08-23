7c89312138ad9492c95044c5b056feaf94e7e3d2
sid-6581 <sid6581@gmail.com>
2024-08-22 22:39:25 -0500

    Add half page scrolling

--- lua/plugins/mini_pick.lua
+++ lua/plugins/mini_pick.lua
@@ -1,3 +1,24 @@
+local make_scroll_half_page = function(direction)
+  local scroll_key = direction == "down" and "<C-d>" or "<C-u>"
+  scroll_key = vim.api.nvim_replace_termcodes(scroll_key, true, true, true)
+  local scroll_fun = function(win_id)
+    vim.api.nvim_win_call(win_id, function() vim.cmd("normal! " .. scroll_key) end)
+  end
+
+  return function()
+    local win_id = require("mini.pick").get_picker_state().windows.main
+    local is_not_main_view = vim.api.nvim_get_current_buf() ~= require("mini.pick").get_picker_state().buffers.main
+    if is_not_main_view then return scroll_fun(win_id) end
+
+    local n_scroll = math.floor(0.5 * vim.api.nvim_win_get_height(win_id))
+    local mappings = require("mini.pick").get_picker_opts().mappings
+    local move_key = direction == "down" and mappings.move_down or mappings.move_up
+    for _ = 1, n_scroll do
+      vim.api.nvim_input(move_key)
+    end
+  end
+end
+
 return {
   "echasnovski/mini.pick",
 
@@ -14,7 +35,7 @@ return {
 
       delete_char = "<BS>",
       delete_char_right = "<Del>",
-      delete_left = "<C-u>",
+      delete_left = "",
       delete_word = "<C-BS>",
 
       mark = "<C-x>",
@@ -33,6 +54,9 @@ return {
       scroll_left = "<C-h>",
       scroll_right = "<C-l>",
       scroll_up = "<C-b>",
+
+      scroll_down_half = { char = "<C-d>", func = make_scroll_half_page("down") },
+      scroll_up_half = { char = "<C-u>", func = make_scroll_half_page("up") },
 
       stop = "<Esc>",
 
