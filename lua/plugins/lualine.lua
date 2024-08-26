e6769b40fbbc2877d7ea698118a89476b91cc52a
sid-6581 <sid6581@gmail.com>
2024-08-26 16:08:33 -0500

    Remove noice component from lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -59,11 +59,6 @@ return {
         },
         lualine_x = {
           {
-            require("noice").api.status.command["get"],
-            cond = require("noice").api.status.command["has"],
-            color = { fg = "#ff9e64" },
-          },
-          {
             require("noice").api.status.mode["get"],
             cond = require("noice").api.status.mode["has"],
             color = { fg = "#ff9e64" },
