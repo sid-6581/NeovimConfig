52fb747a7fcca80ced24e1c65244078e8936ba5b
sid-6581 <sid6581@gmail.com>
2024-08-27 21:45:15 -0500

    Remove notify

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -7,7 +7,6 @@ return {
       border = "single",
       get_config = function(opts)
         opts.prompt = opts.prompt:gsub("Neo%-tree Popup\n", "")
-        vim.notify(vim.inspect(opts))
         return {
           insert_only = #(opts.default or "") == 0,
           start_in_insert = #(opts.default or "") == 0,
