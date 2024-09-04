367c3ce7c254ccf5987793890350c2823f210a7d
sid-6581 <sid6581@gmail.com>
2024-09-04 15:45:20 -0500

    Add autocmd for turning off hlsearch

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,4 +1,3 @@
-local util = require("util")
 local winbuf = require("util.winbuf")
 
 -- Normalize paths for Windows buffers
@@ -17,44 +16,58 @@ local winbuf = require("util.winbuf")
 -- end
 
 -- Check if we need to reload the file when it changed
-vim.api.nvim_create_autocmd({
-  "FocusGained",
-  "TermClose",
-  "TermLeave",
-}, {
-  command = "checktime",
-})
+vim.api.nvim_create_autocmd(
+  { "FocusGained", "TermClose", "TermLeave" },
+  {
+    command = "checktime",
+  }
+)
 
 -- Turn off filetype indent for vue, because it uses HTML indent which doesn't work with pug
-vim.api.nvim_create_autocmd({
-  "FileType",
-}, {
-  pattern = { "vue" },
-  callback = function()
-    vim.opt_local.indentkeys = nil
-  end,
-})
+vim.api.nvim_create_autocmd(
+  { "FileType" },
+  {
+    pattern = { "vue" },
+    callback = function()
+      vim.opt_local.indentkeys = nil
+    end,
+  }
+)
 
 -- Turn off comment-related formatting and automatic inserting of comment leaders
-vim.api.nvim_create_autocmd({
-  "BufWinEnter",
-  "FileType",
-}, {
-  callback = function()
-    vim.opt.formatoptions:remove({ "c", "r", "o" })
-  end,
-})
+vim.api.nvim_create_autocmd(
+  { "BufWinEnter", "FileType" },
+  {
+    callback = function()
+      vim.opt.formatoptions:remove({ "c", "r", "o" })
+    end,
+  }
+)
 
 -- Delete hidden no name buffers every time a new one is opened
-vim.api.nvim_create_autocmd({
-  "BufLeave",
-}, {
-  callback = function()
-    winbuf.buffers_run(
-      { noname = true, hidden = true },
-      function(bufnr)
-        vim.api.nvim_buf_delete(bufnr, { force = true })
+vim.api.nvim_create_autocmd(
+  { "BufLeave" },
+  {
+    callback = function()
+      winbuf.buffers_run(
+        { noname = true, hidden = true },
+        function(bufnr)
+          vim.api.nvim_buf_delete(bufnr, { force = true })
+        end
+      )
+    end,
+  }
+)
+
+-- Turn off hlsearch automatically
+vim.api.nvim_create_autocmd(
+  { "CursorMoved", "CursorMovedI" },
+  {
+    group = vim.api.nvim_create_augroup("auto-hlsearch", { clear = true }),
+    callback = function()
+      if vim.v.hlsearch == 1 and vim.fn.searchcount().exact_match == 0 then
+        vim.schedule(function() vim.cmd.nohlsearch() end)
       end
-    )
-  end,
-})
+    end,
+  }
+)
