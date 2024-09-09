aeec8e68ebcd3fc0c70182e5f9b9ffe56ddafa84
sid-6581 <sid6581@gmail.com>
2024-09-09 13:45:36 -0500

    Remove some markview settings

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -26,28 +26,6 @@ return {
     modes = { "n", "i", "no", "c" },
     hybrid_modes = { "n", "i" },
 
-    headings = {
-      shift_width = 0,
-      heading_1 = {
-        icon = "󰉫 ",
-      },
-      heading_2 = {
-        icon = "󰉬 ",
-      },
-      heading_3 = {
-        icon = "󰉭 ",
-      },
-      heading_4 = {
-        icon = "󰉮 ",
-      },
-      heading_5 = {
-        icon = "󰉯 ",
-      },
-      heading_6 = {
-        icon = "󰉰 ",
-      },
-    },
-
     callbacks = {
       on_enable = function(_, win)
         vim.wo[win].conceallevel = 2
