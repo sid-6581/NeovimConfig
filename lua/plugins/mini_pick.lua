6352b60f6d4bd10a62382888058bf0cb00e30c4c
sid-6581 <sid6581@gmail.com>
2024-10-01 13:43:25 -0500

    Tweak pickers

--- lua/plugins/mini_pick.lua
+++ lua/plugins/mini_pick.lua
@@ -31,10 +31,42 @@ return {
     mappings = {
       caret_left = "<Left>",
       caret_right = "<Right>",
-
       choose = "<CR>",
       choose_in_split = "<C-s>",
       choose_in_tabpage = "",
+      choose_in_vsplit = "<C-v>",
+      choose_marked = "<M-CR>",
+      delete_char = "<BS>",
+      delete_char_right = "<Del>",
+      delete_left = "",
+      delete_word = "<C-BS>",
+      mark = "<C-x>",
+      mark_all = "<C-a>",
+      move_down = "<C-j>",
+      move_start = "<C-g>",
+      move_up = "<C-k>",
+      paste = "<C-r>",
+      refine = "<C-Space>",
+      refine_marked = "<M-Space>",
+      scroll_down = "<C-f>",
+      scroll_down_half = { char = "<C-d>", func = make_scroll_half_page("down") },
+      scroll_left = "<C-h>",
+      scroll_right = "<C-l>",
+      scroll_up = "<C-b>",
+      scroll_up_half = { char = "<C-u>", func = make_scroll_half_page("up") },
+      stop = "<Esc>",
+      toggle_info = "<F1>",
+      toggle_preview = "<C-p>",
+
+      to_quickfix = {
+        char = "<C-q>",
+        func = function()
+          local pick = require("mini.pick")
+          local items = pick.get_picker_matches().all or {}
+          pick.default_choose_marked(items)
+          return true
+        end,
+      },
 
       choose_in_tabpage_fixed = {
         char = "<C-t>",
@@ -51,61 +83,12 @@ return {
           return true
         end,
       },
-
-      choose_in_vsplit = "<C-v>",
-      choose_marked = "<M-CR>",
-
-      delete_char = "<BS>",
-      delete_char_right = "<Del>",
-      delete_left = "",
-      delete_word = "<C-BS>",
-
-      mark = "<C-x>",
-      mark_all = "<C-a>",
-
-      move_down = "<C-j>",
-      move_start = "<C-g>",
-      move_up = "<C-k>",
-
-      paste = "<C-r>",
-
-      refine = "<C-Space>",
-      refine_marked = "<M-Space>",
-
-      scroll_down = "<C-f>",
-      scroll_left = "<C-h>",
-      scroll_right = "<C-l>",
-      scroll_up = "<C-b>",
-
-      scroll_down_half = {
-        char = "<C-d>",
-        func = make_scroll_half_page("down"),
-      },
-      scroll_up_half = {
-        char = "<C-u>",
-        func = make_scroll_half_page("up"),
-      },
-
-      stop = "<Esc>",
-
-      toggle_info = "<F1>",
-      toggle_preview = "<C-p>",
-
-      to_quickfix = {
-        char = "<C-q>",
-        func = function()
-          local pick = require("mini.pick")
-          local items = pick.get_picker_matches().all or {}
-          pick.default_choose_marked(items)
-          return true
-        end,
-      },
     },
 
     window = {
       config = function()
-        local height = math.floor(0.618 * vim.o.lines)
-        local width = math.floor(0.618 * vim.o.columns)
+        local height = math.floor(0.65 * vim.o.lines)
+        local width = math.floor(0.65 * vim.o.columns)
         return {
           anchor = "NW",
           height = height,
@@ -116,4 +99,9 @@ return {
       end,
     },
   },
+
+  config = function(_, opts)
+    require("mini.pick").setup(opts)
+    vim.api.nvim_set_hl(0, "MiniPickMatchRanges", { link = "Identifier" })
+  end,
 }
