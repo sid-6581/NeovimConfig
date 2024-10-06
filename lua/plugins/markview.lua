3223fedad9075a816b644d79a65ba4008cfb6ba3
sid-6581 <sid6581@gmail.com>
2024-10-05 23:11:29 -0500

    Tweak markview

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -6,13 +6,6 @@ return {
   opts = {
     modes = { "n", "i", "no", "c" },
     hybrid_modes = { "n", "i" },
-
-    callbacks = {
-      on_enable = function(_, win)
-        vim.wo[win].conceallevel = 2
-        vim.wo[win].concealcursor = ""
-      end,
-    },
   },
 
   config = function(_, opts)
