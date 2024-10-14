e6bf70e322d267b3364701a1d9f299b698de16a2
sid-6581 <sid6581@gmail.com>
2024-10-14 15:27:06 -0500

    Remove cliff

--- lua/plugins/cliff.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "Rentib/cliff.nvim",
-  event = "VeryLazy",
-
-  keys = {
-    { "<A-J>", function() require("cliff").go_down() end, mode = { "n", "x", "o" }, desc = "Down [cliff]" },
-    { "<A-K>", function() require("cliff").go_up() end, mode = { "n", "x", "o" }, desc = "Up [cliff]" },
-  },
-}
