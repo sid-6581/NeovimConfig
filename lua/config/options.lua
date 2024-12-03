3a14644a6d3da5109c69b33b3603099280d0edcb
sid-6581 <sid6581@gmail.com>
2024-12-03 12:41:17 -0600

    Disable rust ft maps

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,6 +11,7 @@ vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
+vim.g.no_rust_maps = 1
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
