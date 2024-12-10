379876eef99a6d31ec128ba32fe1f1cc01fca30a
sid-6581 <sid6581@gmail.com>
2024-12-10 14:04:47 -0600

    Use fork of endwise

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -6,7 +6,7 @@ return {
 
   dependencies = {
     "nvim-treesitter/nvim-treesitter-textobjects",
-    "RRethy/nvim-treesitter-endwise",
+    "metiulekm/nvim-treesitter-endwise",
     "RRethy/nvim-treesitter-textsubjects",
   },
 
