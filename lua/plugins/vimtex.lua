4de3a22e86e2c60f25b6bc66f5fbdc8b4d46d1f8
sid-6581 <sid6581@gmail.com>
2025-01-26 17:04:22 -0600

    Tweak vimtex

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -6,19 +6,11 @@ return {
   init = function()
     vim.g.vimtex_view_method = "zathura_simple"
     vim.g.vimtex_syntax_conceal = {
-      accents = 1,
-      ligatures = 1,
-      cites = 1,
-      fancy = 1,
-      spacing = 1,
-      greek = 1,
-      math_bounds = 1,
-      math_delimiters = 1,
-      math_fracs = 1,
-      math_super_sub = 1,
-      math_symbols = 1,
       sections = 1,
-      styles = 1,
+    }
+    vim.g.vimtex_compiler_latexmk = {
+      aux_dir = "build",
+      out_dir = "build",
     }
 
     vim.api.nvim_create_autocmd({ "FileType" }, {
