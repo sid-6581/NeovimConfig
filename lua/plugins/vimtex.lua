cb9c59552191b8f374a4bce85f1062422e630926
sid-6581 <sid6581@gmail.com>
2025-02-09 19:04:38 -0600

    Disable vimtex highlighting

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -4,6 +4,9 @@ return {
   ft = "tex",
 
   init = function()
+    vim.g.vimtex_syntax_enabled = 0
+    vim.g.vimtex_syntax_conceal_disable = 1
+
     vim.g.vimtex_view_method = "zathura_simple"
     vim.g.vimtex_syntax_conceal = {
       sections = 1,
