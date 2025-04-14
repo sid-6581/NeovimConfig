b1f22e8efe4689a5b1687b0c4ef421af28d16273
sid-6581 <sid6581@gmail.com>
2025-04-14 14:13:38 -0500

    Add colorize

--- lua/util/winbuf.lua
+++ lua/util/winbuf.lua
@@ -197,6 +197,26 @@ function M.close_window_or_buffer()
   vim.cmd.bdelete()
 end
 
+function M.colorize()
+  vim.wo.number = false
+  vim.wo.relativenumber = false
+  vim.wo.statuscolumn = ""
+  vim.wo.signcolumn = "no"
+  vim.opt.listchars = { space = " " }
+
+  local buf = vim.api.nvim_get_current_buf()
+  local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+
+  while #lines > 0 and vim.trim(lines[#lines]) == "" do
+    lines[#lines] = nil
+  end
+
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, {})
+  vim.api.nvim_chan_send(vim.api.nvim_open_term(buf, {}), table.concat(lines, "\r\n"))
+  vim.keymap.set("n", "q", "<cmd>q<cr>", { silent = true, buffer = buf })
+  vim.api.nvim_create_autocmd("TermEnter", { buffer = buf, command = "stopinsert" })
+end
+
 return M
 
 --- @class BufFilter
