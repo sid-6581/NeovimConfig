9ecf971b15849f42c7f703a176c59ef09aee181e
sid-6581 <sid6581@gmail.com>
2025-05-28 13:33:18 -0500

    Tweak tailwind

--- lsp/tailwindcss.lua
+++ lsp/tailwindcss.lua
@@ -1,2 +1,17 @@
 return {
+  settings = {
+    tailwindCSS = {
+      classAttributes = {
+        "class",
+        "className",
+        "class:list",
+        "classList",
+        "ngClass",
+        "header-class",
+        "row-class",
+        "table-class",
+        "body-class",
+      },
+    },
+  },
 }
