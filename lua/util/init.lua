09f56ace869609fae1a93f657983cb108bb35ad9
sid-6581 <sid6581@gmail.com>
2025-05-29 13:21:29 -0500

    Fix folding

--- lua/util/init.lua
+++ lua/util/init.lua
@@ -2,22 +2,18 @@ local M = {}
 
 -- Closes all folds with a given fold level. Does not close folds inside the fold recursively.
 function M.close_folds_with_level(level)
-  local win_view = vim.fn.winsaveview()
   local line_count = vim.api.nvim_buf_line_count(0)
   local line = 1
 
   while line <= line_count do
-    if vim.fn.foldlevel(line) == level then
-      vim.api.nvim_win_set_cursor(0, { line, 0 })
-      vim.cmd.normal({ "zc", bang = true })
+    if vim.fn.foldclosed(line) == -1 and vim.fn.foldlevel(line) == level then
+      vim.cmd.foldclose({ range = { line } })
       local end_line = vim.fn.foldclosedend(line)
       line = end_line > 0 and (end_line + 1) or (line + 1)
     else
       line = line + 1
     end
   end
-
-  if win_view then vim.fn.winrestview(win_view) end
 end
 
 -- Closes all folds with a given treesitter textobject. Does not close folds inside the fold recursively.
