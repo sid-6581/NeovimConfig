49ae4c56626da3f69770765c94efe42f2be0dff1
sid-6581 <sid6581@gmail.com>
2025-07-12 18:12:46 -0500

    chore: add schemas

--- lsp/yamlls.lua
+++ lsp/yamlls.lua
@@ -2,7 +2,31 @@ return {
   settings = {
     yaml = {
       schemaStore = { enable = false, url = "" },
-      schemas = require("schemastore").yaml.schemas(),
+      schemas = require("schemastore").yaml.schemas(
+        {
+          extra = {
+            {
+              name = "Argo Workflows",
+              description = "Argo Workflows",
+              url = "https://raw.githubusercontent.com/argoproj/argo-workflows/main/api/jsonschema/schema.json",
+              fileMatch = {
+                "*-workflowtemplate.yaml",
+                "*-workflow.yaml",
+              },
+            },
+            {
+              name = "Argo Events",
+              description = "Argo Events",
+              url = "https://raw.githubusercontent.com/argoproj/argo-events/refs/heads/master/api/jsonschema/schema.json",
+              fileMatch = {
+                "*-sensor.yaml",
+                "*-eventsource.yaml",
+                "*-eventbus.yaml",
+              },
+            },
+          },
+        }
+      ),
     },
   },
 }
