f2807b0846593bc1f816f7555407c41b0d5b0ecf
sid-6581 <sid6581@gmail.com>
2025-09-14 19:15:52 -0500

    Add kubernetes schemas

--- lsp/yamlls.lua
+++ lsp/yamlls.lua
@@ -2,31 +2,53 @@ return {
   settings = {
     yaml = {
       schemaStore = { enable = false, url = "" },
-      schemas = require("schemastore").yaml.schemas(
+      schemas = vim.tbl_extend("force",
         {
-          extra = {
-            {
-              name = "Argo Workflows",
-              description = "Argo Workflows",
-              url = "https://raw.githubusercontent.com/argoproj/argo-workflows/main/api/jsonschema/schema.json",
-              fileMatch = {
-                "*-workflowtemplate.yaml",
-                "*-workflow.yaml",
-                "*-cronworkflow.yaml",
-              },
-            },
-            {
-              name = "Argo Events",
-              description = "Argo Events",
-              url = "https://raw.githubusercontent.com/argoproj/argo-events/refs/heads/master/api/jsonschema/schema.json",
-              fileMatch = {
-                "*-sensor.yaml",
-                "*-eventsource.yaml",
-                "*-eventbus.yaml",
-              },
-            },
+          kubernetes = {
+            "*-backendtrafficpolicy.yaml",
+            "*-clusterrole.yaml",
+            "*-clusterrolebinding.yaml",
+            "*-configmap.yaml",
+            "*-deployment.yaml",
+            "*-gateway.yaml",
+            "*-httproute.yaml",
+            "*-namespace.yaml",
+            "*-persistentvolume.yaml",
+            "*-persistentvolumeclaim.yaml",
+            "*-role.yaml",
+            "*-rolebinding.yaml",
+            "*-secret.yaml",
+            "*-service.yaml",
+            "*-storageclass.yaml",
+            "*-tcproute.yaml",
           },
-        }
+        },
+        require("schemastore").yaml.schemas(
+          {
+            extra = {
+              {
+                name = "Argo Workflows",
+                description = "Argo Workflows",
+                url = "https://raw.githubusercontent.com/argoproj/argo-workflows/main/api/jsonschema/schema.json",
+                fileMatch = {
+                  "*-workflowtemplate.yaml",
+                  "*-workflow.yaml",
+                  "*-cronworkflow.yaml",
+                },
+              },
+              {
+                name = "Argo Events",
+                description = "Argo Events",
+                url = "https://raw.githubusercontent.com/argoproj/argo-events/refs/heads/master/api/jsonschema/schema.json",
+                fileMatch = {
+                  "*-sensor.yaml",
+                  "*-eventsource.yaml",
+                  "*-eventbus.yaml",
+                },
+              },
+            },
+          }
+        )
       ),
     },
   },
