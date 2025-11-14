a79d3837a36228403d872613fbb38813f3356b7b
sid-6581 <sid6581@gmail.com>
2025-11-14 17:32:37 -0600

    Fix root dir for frontend

--- lsp/eslint.lua
+++ lsp/eslint.lua
@@ -2,4 +2,9 @@ return {
   on_attach = function(client, _bufnr)
     client.server_capabilities.documentFormattingProvider = true
   end,
+  root_dir = function(bufnr, on_dir)
+    local root_markers = { { "package-lock.json", "yarn.lock", "pnpm-lock.yaml", "bun.lockb", "bun.lock" }, { ".git" } }
+    local project_root = vim.fs.root(bufnr, root_markers) or vim.fn.getcwd()
+    on_dir(project_root)
+  end,
 }
--- lsp/vtsls.lua
+++ lsp/vtsls.lua
@@ -9,6 +9,12 @@ return {
     "vue",
   },
 
+  root_dir = function(bufnr, on_dir)
+    local root_markers = { { "package-lock.json", "yarn.lock", "pnpm-lock.yaml", "bun.lockb", "bun.lock" }, { ".git" } }
+    local project_root = vim.fs.root(bufnr, root_markers) or vim.fn.getcwd()
+    on_dir(project_root)
+  end,
+
   settings = {
     vtsls = {
       autoUseWorkspaceTsdk = true,
