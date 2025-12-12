a26900c95b0cfb924b96b41177a63f1430e7234f
sid-6581 <sid6581@gmail.com>
2025-12-12 13:38:20 -0600

    Cleanup mini.files

--- lua/plugins/mini_files.lua
+++ lua/plugins/mini_files.lua
@@ -1,30 +1,6 @@
--- Open file in split.
-local open_in = function(cmd)
-  local new_target_window = vim.api.nvim_win_call(
-    require("mini.files").get_explorer_state().target_window or 0,
-    function()
-      vim.cmd(cmd)
-      return vim.api.nvim_get_current_win()
-    end
-  )
+local M = {}
 
-  require("mini.files").set_target_window(new_target_window)
-  require("mini.files").go_in({ close_on_file = true })
-end
-
--- Smart "go out" that lets the cursor move left normally if it's not on the
--- first character of the file name, otherwise calls go_out().
-local smart_go_out = function()
-  local _, col = unpack(vim.api.nvim_win_get_cursor(0))
-  vim.cmd.normal({ "h", bang = true })
-  require("mini.files").refresh()
-  local _, new_col = unpack(vim.api.nvim_win_get_cursor(0))
-  if col == new_col then
-    require("mini.files").go_out()
-  end
-end
-
-return {
+M.spec = {
   "nvim-mini/mini.files",
   event = "VeryLazy",
 
@@ -64,9 +40,9 @@ return {
           require("which-key").add({
             buffer = event.data.buf_id,
             { "<Esc>", function() require("mini.files").close() end, desc = "Close [mini.files]" },
-            { "<C-s>", function() open_in("split") end, desc = "Open in horizontal split [mini.files]" },
-            { "<C-v>", function() open_in("vsplit") end, desc = "Open in vertical split [mini.files]" },
-            { "h", function() smart_go_out() end, desc = "Go out [mini.files]" },
+            { "<C-s>", function() M.open_in("split") end, desc = "Open in horizontal split [mini.files]" },
+            { "<C-v>", function() M.open_in("vsplit") end, desc = "Open in vertical split [mini.files]" },
+            { "h", function() M.smart_go_out() end, desc = "Go out [mini.files]" },
             { "<A-h>", "h", desc = "Left [mini.files]" },
             { "<A-j>", "j", desc = "Down [mini.files]" },
             { "<A-k>", "k", desc = "Up [mini.files]" },
@@ -99,3 +75,31 @@ return {
     -- })
   end,
 }
+
+-- Open file in split.
+M.open_in = function(cmd)
+  local new_target_window = vim.api.nvim_win_call(
+    require("mini.files").get_explorer_state().target_window or 0,
+    function()
+      vim.cmd(cmd)
+      return vim.api.nvim_get_current_win()
+    end
+  )
+
+  require("mini.files").set_target_window(new_target_window)
+  require("mini.files").go_in({ close_on_file = true })
+end
+
+-- Smart "go out" that lets the cursor move left normally if it's not on the
+-- first character of the file name, otherwise calls go_out().
+M.smart_go_out = function()
+  local _, col = unpack(vim.api.nvim_win_get_cursor(0))
+  vim.cmd.normal({ "h", bang = true })
+  require("mini.files").refresh()
+  local _, new_col = unpack(vim.api.nvim_win_get_cursor(0))
+  if col == new_col then
+    require("mini.files").go_out()
+  end
+end
+
+return M
