4fb961adef3086afc57bba13b4269158ccfdb263
sid-6581 <sid6581@gmail.com>
2026-02-28 11:21:27 -0600

    Update colorizer

--- lua/plugins/nvim-colorizer.lua
+++ lua/plugins/nvim-colorizer.lua
@@ -3,11 +3,7 @@ return {
   event = "VeryLazy",
 
   opts = {
-    user_default_options = {
-      names = false,
-      -- mode = "virtualtext",
-      -- -- virtualtext_inline = true,
-      -- virtualtext = "",
+    options = {
     },
   },
 }
