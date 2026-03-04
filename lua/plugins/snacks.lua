5c6807bce1ac974a6d3a2466b2d63a11d1ce6ae1
sid-6581 <sid6581@gmail.com>
2026-03-04 13:35:21 -0600

    Disable indent in markdown

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -90,6 +90,15 @@ return {
         },
         hl = "GruvboxBg4",
       },
+
+      filter = function(buf, win)
+        return (
+          vim.g.snacks_indent ~= false and
+          vim.b[buf].snacks_indent ~= false and
+          vim.bo[buf].buftype == "" and
+          vim.bo[buf].filetype ~= "markdown"
+        )
+      end,
     },
 
     input = {
